cecde43a9c36b9bfd63b059ddb663ab4ad3c9582
Kevnel <kevichen510@gmail.com>
2024-05-21 13:34:29 -0700

    Add project files.

---
+++ Dodge/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Dodge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player1ScoreOutput = new System.Windows.Forms.Label();
+            this.player2ScoreOutput = new System.Windows.Forms.Label();
+            this.timerOutput = new System.Windows.Forms.Label();
+            this.winnerOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player1ScoreOutput
+            // 
+            this.player1ScoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1ScoreOutput.Location = new System.Drawing.Point(187, 226);
+            this.player1ScoreOutput.Name = "player1ScoreOutput";
+            this.player1ScoreOutput.Size = new System.Drawing.Size(100, 23);
+            this.player1ScoreOutput.TabIndex = 4;
+            this.player1ScoreOutput.Text = "label1";
+            this.player1ScoreOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player2ScoreOutput
+            // 
+            this.player2ScoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2ScoreOutput.Location = new System.Drawing.Point(399, 226);
+            this.player2ScoreOutput.Name = "player2ScoreOutput";
+            this.player2ScoreOutput.Size = new System.Drawing.Size(100, 23);
+            this.player2ScoreOutput.TabIndex = 5;
+            this.player2ScoreOutput.Text = "label2";
+            this.player2ScoreOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerOutput
+            // 
+            this.timerOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerOutput.Location = new System.Drawing.Point(279, 9);
+            this.timerOutput.Name = "timerOutput";
+            this.timerOutput.Size = new System.Drawing.Size(128, 23);
+            this.timerOutput.TabIndex = 6;
+            this.timerOutput.Text = "label3";
+            this.timerOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winnerOutput
+            // 
+            this.winnerOutput.BackColor = System.Drawing.Color.Transparent;
+            this.winnerOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerOutput.Location = new System.Drawing.Point(278, 115);
+            this.winnerOutput.Name = "winnerOutput";
+            this.winnerOutput.Size = new System.Drawing.Size(128, 23);
+            this.winnerOutput.TabIndex = 7;
+            this.winnerOutput.Text = "winnerOutput";
+            this.winnerOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winnerOutput.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 261);
+            this.Controls.Add(this.winnerOutput);
+            this.Controls.Add(this.timerOutput);
+            this.Controls.Add(this.player2ScoreOutput);
+            this.Controls.Add(this.player1ScoreOutput);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player1ScoreOutput;
+        private System.Windows.Forms.Label player2ScoreOutput;
+        private System.Windows.Forms.Label timerOutput;
+        private System.Windows.Forms.Label winnerOutput;
+    }
+}
+
---
+++ Dodge/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Dodge
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.victory);
+        SoundPlayer soundPlayer2 = new SoundPlayer(Properties.Resources.blown);
+        SoundPlayer soundPlayer3 = new SoundPlayer(Properties.Resources.scored);
+        SoundPlayer soundPlayer4 = new SoundPlayer(Properties.Resources.rocketThrust);
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Font drawFont = new Font("Arial", 10, FontStyle.Bold);
+
+        Rectangle player1rocket = new Rectangle(0, 50, 50, 20);
+        Rectangle player2rocket = new Rectangle(0, 180, 50, 20);
+        int playerSpeed = 10;
+
+        new List<Rectangle> ballList = new List<Rectangle>();
+        new List<int> ballSpeeds = new List<int>();
+
+        int player1Score = 0;
+        int player2Score = 0;
+        int time = 1000;
+
+        bool aPressed = false;
+        bool dPressed = false;
+        bool leftPressed = false;
+        bool rightPressed = false;
+
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush grayBrush = new SolidBrush(Color.Gray);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+
+        Random randGen = new Random();
+        int randValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
+                    break;
+                case Keys.Left:
+                    leftPressed = true;
+                    break;
+                case Keys.Right:
+                    rightPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+                case Keys.Left:
+                    leftPressed = false;
+                    break;
+                case Keys.Right:
+                    rightPressed = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            time--;
+
+            for (int i = 0; i < ballList.Count; i++)
+            {
+                int y = ballList[i].Y + ballSpeeds[i];
+
+                ballList[i] = new Rectangle(ballList[i].X, y, 10, 20);
+            }
+
+            if (aPressed == true && player1rocket.X > 0)
+            {
+                player1rocket.X -= playerSpeed;
+                soundPlayer4.Play();
+            }
+            else if (dPressed == true)
+            {
+                player1rocket.X += playerSpeed;
+                soundPlayer4.Play();
+            }
+            if (leftPressed == true && player2rocket.X > 0)
+            {
+                player2rocket.X -= playerSpeed;
+                soundPlayer4.Play();
+            }
+            else if (rightPressed == true)
+            {
+                player2rocket.X += playerSpeed;
+                soundPlayer4.Play();
+            }
+
+            randValue = randGen.Next(0, 100);
+            if (randValue < 15)
+            {
+                randValue = randGen.Next(100, 600);
+                Rectangle ball = new Rectangle(randValue, -10, 10, 20);
+                ballList.Add(ball);
+                ballSpeeds.Add(randGen.Next(5, 15));
+            }
+
+            for (int i = 0; i < ballList.Count; i++)
+            {
+                if (ballList[i].Y > this.Height)
+                {
+                    ballList.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                }
+            }
+
+            for (int i = 0; i < ballList.Count; i++)
+            {
+                if (ballList[i].IntersectsWith(player1rocket))
+                {
+                    soundPlayer2.Play();
+                    player1rocket.X = 0;
+                }
+                else if (ballList[i].IntersectsWith(player2rocket))
+                {
+                    soundPlayer2.Play();
+                    player2rocket.X = 0;
+                }
+            }
+
+            if (player1rocket.X + player1rocket.Width > this.Width)
+            {
+                soundPlayer3.Play();
+                player1Score++;
+                player1rocket.X = 0;
+            }
+            else if (player2rocket.X + player2rocket.Width > this.Width)
+            {
+                soundPlayer3.Play();
+                player2Score++;
+                player2rocket.X = 0;
+            }
+
+            if (time == 0)
+            {
+                gameTimer.Stop();
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            timerOutput.Text = $"Time Left: {time}";
+            player1ScoreOutput.Text = $"Score: {player1Score}";
+            player2ScoreOutput.Text = $"Score: {player2Score}";
+
+            e.Graphics.FillRectangle(grayBrush, 0, 125, this.Width, 5);
+
+            //draw player rockets
+            e.Graphics.FillRectangle(blackBrush, player1rocket);
+            e.Graphics.FillRectangle(blackBrush, player2rocket);
+
+            //draw balls
+            for (int i = 0; i < ballList.Count; i++)
+            {
+                e.Graphics.FillRectangle(redBrush, ballList[i]);
+            }
+
+            if (time == 0 && player1Score > player2Score)
+            {
+                winnerOutput.Visible = true;
+                winnerOutput.Text = "Player 1 Wins!";
+                soundPlayer.Play();
+            }
+            else if (time == 0 && player2Score > player1Score)
+            {
+                winnerOutput.Visible = true;
+                winnerOutput.Text = "Player 2 Wins!";
+                soundPlayer.Play();
+            }
+            else if (time == 0 && player2Score == player1Score)
+            {
+                winnerOutput.Visible = true;
+                winnerOutput.Text = "It's a tie!";
+                soundPlayer.Play();
+            }
+        }
+    }
+}
